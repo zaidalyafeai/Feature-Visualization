a3ee5690696200aacafb1579d67566c1f8eb0986
zaidalyafeai <alyafey22@gmail.com>
2019-06-22 09:37:44 +0300

    clean up the code and set the latent vector is random

--- runway_model.py
+++ runway_model.py
@@ -1,5 +1,7 @@
 # Imports
 
+
+import os
 import numpy as np
 import lucid.modelzoo.vision_models as models
 from lucid.misc.io import show
@@ -8,8 +10,10 @@ import lucid.optvis.param as param
 import lucid.optvis.render as render
 import lucid.optvis.transform as transform
 import time
+import tensorflow as tf
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 import runway
-
+import random
 
 @runway.setup(options={})
 def setup(opts):
@@ -19,18 +23,30 @@ def setup(opts):
 
 
 generate_inputs = {
-    'z': runway.vector(length=1, sampling_std=0.5)
+    'z': runway.vector(length=1, sampling_std=0.5),
+    'steps': runway.number(min=64, max=512, step=64, default=256)
 }
 
 @runway.command('generate', inputs=generate_inputs, outputs={'image': runway.image})
 def convert(model, inputs):
     start = time.time()
-    latents = int(np.clip(np.float(inputs['z']) * 1000, 0, 507))
+
+    #set up parameters
+    #neuron = int(np.clip(np.float(inputs['z']) * 1000, 0, 507))
+    neuron = random.randint(0, 506)
+    steps = int(inputs['steps'])
+
+    #start rendering the images
     param_f = lambda: param.image(512, decorrelate=True)
-    print('The current latents ', latents)
-    output = render.render_vis(model, "mixed4a_pre_relu:"+str(latents), param_f, thresholds=(200,),)
+    output = render.render_vis(model, "mixed4a_pre_relu:"+str(neuron),
+                     param_f, thresholds=(steps,), verbose = False)
+
+    #logging
+    elapsed = time.time() - start
+    print(f'neuron {neuron}: steps: {steps} time: {elapsed}')
+
+    #output results
     image = output[0].squeeze() * 255
-    print('The time is ', time.time() - start)
     return {'image': image.astype('uint8')}
 
 
