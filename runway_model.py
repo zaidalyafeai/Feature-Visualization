14cb44581df1d263f955f5a8794acae6dedb9bcb
zaidalyafeai <alyafey22@gmail.com>
2019-06-23 12:26:02 +0300

    add layers list

--- runway_model.py
+++ runway_model.py
@@ -22,25 +22,31 @@ def setup(opts):
 
 generate_inputs = {
     'z': runway.vector(length=1, sampling_std=0.5),
-    'steps': runway.number(min=64, max=512, step=64, default=256)
+    'layer': runway.category(choices=["mixed3a", "mixed4a", "mixed5a"]
+    , default="mixed4a"),
+    'steps': runway.number(min=64, max=512, step=10, default=128)
 }
 
 @runway.command('generate', inputs=generate_inputs, outputs={'image': runway.image})
 def convert(model, inputs):
+
+    num_neurons = {'mixed3a':255, 'mixed4a':507, 'mixed5a':831}
     start = time.time()
 
     #set up parameters
-    neuron = int(np.clip(np.float(inputs['z']) * 1000, 0, 507))
+    layer = inputs['layer']
+    neuron = int(np.clip(np.float(inputs['z']) * 1000, 0, num_neurons[layer]))
     steps = int(inputs['steps'])
+    
 
     #start rendering the images
     param_f = lambda: param.image(512, decorrelate=True)
-    output = render.render_vis(model, "mixed4a_pre_relu:"+str(neuron),
+    output = render.render_vis(model, layer+":"+str(neuron),
                      param_f, thresholds=(steps,), verbose = False)
 
     #logging
     elapsed = time.time() - start
-    print(f'neuron {neuron}: steps: {steps} time: {elapsed}')
+    print(f'layer: {layer} neuron {neuron}: steps: {steps} time: {elapsed}')
 
     #output results
     image = output[0].squeeze() * 255
